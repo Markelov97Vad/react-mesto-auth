a162e4493e8fdbd9b5327798046d7b9ae925d62b
Vadim Markelov <Vedmakmarkelov@yandex.ru>
2023-05-16 22:54:48 +0300

    feat: readme

--- src/components/EditProfilePopup.jsx
+++ src/components/EditProfilePopup.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect } from "react";
 import PopupWithForm from "./PopupWithForm";
 import { CurrentUserContext } from "../contexts/CurrentUserContext";
 import { useForm } from "../hooks/useForm";
